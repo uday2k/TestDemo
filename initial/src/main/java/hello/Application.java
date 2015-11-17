f5c89f5dcdd04a3070eec8fa49fef6929d6910d2
udayoff <udayoff@gmail.com>
2015-11-17 11:55:40 -0800

    Test Change

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -14,6 +14,8 @@ public class Application {
         
         System.out.println("Let's inspect the beans provided by Spring Boot:");
         
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
